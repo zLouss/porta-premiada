4b8051567bc1cbbf04061a64390142487cb86d2e
luis_belo <luis.victor.belo@gmail.com>
2024-04-18 17:35:49 -0300

    atualizacao porta

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Presente from '@/components/Presente'
+import Porta from '@/components/Porta'
+
+export default function Home() {
+  return (
+    <div>
+      <Porta />
+      <Porta selecionada/>
+    </div>
+  );
+}
