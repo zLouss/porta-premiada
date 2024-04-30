a1cf076df583404419f51f0a93f390831af9bfaf
luis_belo <luis.victor.belo@gmail.com>
2024-04-30 13:18:30 -0300

    adicionado ts e evento da porta

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Porta from '@/components/Porta'
+import PortaModel from "../model/porta"
+import { use, useState } from 'react';
+
+export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
+  const [texto, setTexto] = useState("...")
+  
+  return (
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <input type="text" value={texto}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+      {/* <Porta porta={p2} /> */}
+    </div>
+  );
+}
